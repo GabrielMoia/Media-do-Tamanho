11bf89c171fec30a633d974afc88d8544737255f
GabrielMoia <gabrielriosmoia@gmail.com>
2025-09-25 14:22:11 -0300

    Add files via upload

---
+++ media do aluno.py
@@ -0,0 +1,6 @@
+nome = input("Digite seu nome: ")
+tamanho = int(input("Digite o tamanho do aluno em cm: "))
+tamanho2 = int(input("Digite o tamanho do aluno em cm: "))
+
+media = (tamanho + tamanho2) / 2
+print(f"A média dos tamanhos é: {media:.2f} cm")
